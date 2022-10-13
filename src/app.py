c965870c6e7e68fc71e98dc90da42261518ab340
jeffeuxMartin <jeffeuxmlta2020@gmail.com>
2022-10-13 12:24:05 +0800

    終於～雖然很陽春 😂😂🥲🥲🥲

---
+++ src/app.py
@@ -0,0 +1,44 @@
+from snownlp import SnowNLP
+import streamlit as st
+import pandas as pd
+
+st.title("簡易情感分析")
+
+text = st.text_input('請輸入一段話：')
+
+if text:
+    result = {
+        "text": [],
+    "sentiment": [],
+    "emoji": [],
+    }
+    doc_obj = SnowNLP(text)
+    for sent in doc_obj.sentences:
+        result['text'].append(sent)
+        res_sent = SnowNLP(SnowNLP(sent).han)
+        result['sentiment'].append(res_sent.sentiments)
+        if res_sent.sentiments > 0.75:
+            result['emoji'].append("😍🤬")
+        elif res_sent.sentiments > 0.5:
+            result['emoji'].append("😊😡")
+        elif res_sent.sentiments > 0.25:
+            result['emoji'].append("😌🤨")
+        elif res_sent.sentiments > 0.:
+            result['emoji'].append("😐😕")
+        
+    df = pd.DataFrame.from_dict(result)
+    df = df.style.hide(axis="index")
+
+    # CSS to inject contained in a string
+    hide_table_row_index = """
+                <style>
+                thead tr th:first-child {display:none}
+                tbody th {display:none}
+                </style>
+                """
+
+    # Inject CSS with Markdown
+    st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+    # Display a static table
+    st.table(df)
